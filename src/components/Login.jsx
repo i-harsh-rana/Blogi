dfe5911289edb1d835fc9b3bb39304dd0c33fd70
Harsh Rana <harshrana8454@gmail.com>
2024-08-04 20:04:39 +0530

    little change in login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -54,7 +54,7 @@ function Login() {
                 <div className='space-y-5'>
                     <Input
                         label="Email: "
-                        placeholder="Enter your email."
+                        placeholder="Enter your email"
                         type="email"
                         {...register("email", {
                             required: true, 
@@ -68,7 +68,7 @@ function Login() {
                     <Input 
                         label="Password: "
                         type="password"
-                        placeholder="Enter your password."
+                        placeholder="Enter your password"
                         {...register("password"), {
                             require: true
                         }}
